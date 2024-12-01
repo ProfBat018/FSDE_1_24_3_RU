d710e0bb70bb4e88dee8368f566bc9235181a36d
wayne <profbat018@gmail.com>
2024-12-01 21:51:10 +0400

    C# lesson 1

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Console.WriteLine("Hello, World");
+
+// Console.Write("Aloha");
+// Console.Write("Students !!!");
+
+int num1 = 5;
+int num2 = 6;
+Int32 num3 = 7;
+
+string name = "Elvin";
+
+
+// Console.WriteLine(num1 + num2);
+
+Console.WriteLine($"{num1} + {num2} = {num1 + num2}"); // String interpolation
+
+
+
+
+
