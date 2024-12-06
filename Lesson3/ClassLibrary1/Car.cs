5ac9554c6aa1f49ea8b57c5582021f18e943b8a4
wayne <profbat018@gmail.com>
2024-12-06 17:27:34 +0400

    C# lesson 1=3

---
+++ Lesson3/ClassLibrary1/Car.cs
@@ -0,0 +1,24 @@
+﻿using System.Dynamic;
+using System.Runtime.Intrinsics.Arm;
+
+namespace Aloha;
+
+public class Car
+{
+    public string Make;
+    public string Model;
+    private protected string color;
+
+    public static void Foo()
+    {
+        Console.WriteLine("Foo");
+    }
+}
+
+public class GolfCar : Car
+{
+    GolfCar()
+    {
+        this.color = "Red";
+    }
+}
---
+++ Lesson3/Lesson3/Program.cs
@@ -0,0 +1,39 @@
+﻿#region FirstPart
+
+// using Aloha;
+// using static Aloha.Car;
+
+// Aloha.Car c1 = new Car();
+
+// GolfCar gc;
+
+#endregion
+
+
+#region SecondPart
+
+// MyClass a = new();
+// a.name = "Elvin";
+Random a = new();
+a.Next();
+
+// void foo(string name)
+// {
+    // name = "Samir";
+// }
+
+// class MyClass
+// {
+    // public string name;
+// }
+
+
+
+
+
+#endregion
+
+
+
+
+
