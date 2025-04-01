cd9e549966d304d406549f30a92b4f0d970427b4
wayne <profbat018@gmail.com>
2025-04-01 21:44:17 +0400

    Async Lesson

---
+++ ThreadPool/ThreadPool/Program.cs
@@ -0,0 +1,42 @@
+﻿#region Part1
+
+Console.WriteLine("Start of main thread");
+
+var counter = 0;
+object? lockObject = new();
+
+void foo(string name)
+{
+    Console.WriteLine(name);
+    lock (lockObject)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(
+                $"{name}, IsThreadPoolThread: {Thread.CurrentThread.IsThreadPoolThread}, Counter: {counter++}");
+        }
+    }
+}
+
+ThreadPool.QueueUserWorkItem(_ => foo("Thread 1"));
+
+
+var th1 = new Thread(() =>
+{
+    lock (lockObject)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(
+                $"Thread 1, IsThreadPoolThread: {Thread.CurrentThread.IsThreadPoolThread}, Counter: {counter++}");
+        }
+    }
+});
+
+th1.Start();
+
+th1.Join();
+
+Console.WriteLine("End of main thread");
+
+#endregion
