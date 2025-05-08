85a447fdc45396ecd0fd0541433a8f71469479e5
Azimov Elvin Rasul <azimove@itstep.edu.az>
2025-05-08 21:14:28 +0400

    Updated

---
+++ Lesson2/script.js
@@ -0,0 +1,28 @@
+// let greetingHeading = document.getElementById("greeting");
+
+// greetingHeading.innerText = "Hello, World!";
+// greetingHeading.innerHTML = "Hello, <span style='color:blue'>World</span>!";
+
+// greetingHeading.style.color = "blue";
+
+
+// let form = document.getElementsByClassName("form");
+
+let form = document.getElementsByTagName("form");
+
+form[0].onsubmit = function (event) {
+    event.preventDefault(); // предотвращаем обновление страницы
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+
+    // alert("Имя: " + name + "\nEmail: " + email);
+
+    let greetingMessage = document.createElement("p");
+
+    greetingMessage.innerText = "Имя: " + name + "\nEmail: " + email;
+    greetingMessage.style.color = "blue";
+
+    document.body.appendChild(greetingMessage);
+
+}
